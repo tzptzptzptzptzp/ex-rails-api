112397d48d352a38faf50ee100ebe36361dd60a1
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-16 23:31:46 +0900

    🐛 APIのルーティング設定を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'posts/index'
-      get 'posts/show'
-      get 'posts/create'
-      get 'posts/update'
-      get 'posts/destroy'
+      resources :posts, only: [:index, :show, :create, :update, :destroy]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
