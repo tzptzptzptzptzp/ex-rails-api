e372a563b8167e25129c7d232a94737d9b18cf52
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-16 23:09:51 +0900

    🔧 コントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'posts/index'
+      get 'posts/show'
+      get 'posts/create'
+      get 'posts/update'
+      get 'posts/destroy'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
